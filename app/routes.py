0b49da5029f71b4270eff768c6292d86a7bf1f73
Batfisch <Batfisch@gmx.at>
2018-12-04 12:10:40 +0100

    html files added + templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,28 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
+
 def index():
-    return "Hello, World!"
+    user ={'username':'Patrick'}
+    posts =[
+
+        {
+            'author': {'username':'Max'},
+            'body' : 'Heute ist Dienstag'
+
+        },
+        {
+            'author': {'username':'Peter'},
+            'body': 'Morgen ist Mittwoch'
+
+
+        }
+    ]
+
+    return render_template('index.html',title='Home', user=user, posts=posts)
+
+
+
